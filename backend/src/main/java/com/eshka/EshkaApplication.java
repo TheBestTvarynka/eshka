21c9a8231fd54a781a488159f7d09741ae2f23a1
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-09-19 14:27:16 +0300

    init backend and frontend

---
+++ backend/src/main/java/com/eshka/EshkaApplication.java
@@ -0,0 +1,13 @@
+package com.eshka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshkaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eshka/EshkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
