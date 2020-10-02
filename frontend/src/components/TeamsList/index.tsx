5e3ddc16a9a8429c141ed4241cafc40a827f9ff7
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-10-02 11:37:44 +0300

    improve style of TeamsList

--- frontend/src/components/TeamsList/index.tsx
+++ frontend/src/components/TeamsList/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './styles.module.sass';
 
+const selectedTeam = 1;
+
+const teamsMock = [
+  { title: 'BananasTornadas', membersCount: 9 },
+  { title: 'MizarField', membersCount: 28 },
+  { title: 'Flowers', membersCount: 31 },
+  { title: 'CloudsOfTheGod', membersCount: 15 }
+];
+
 const TeamsList = () => {
   return (
     <div className={styles.teams}>
-      Team1
-      Team2
+      {teamsMock.map((team, index) => (
+        <div className={`${styles.team} ${index === selectedTeam ? styles.selected : styles.simple}`}>
+          <span className={styles.title}>{team.title}</span>
+          <span className={styles.members_count}>{team.membersCount} member(s)</span>
+        </div>
+      ))}
     </div>
   );
 }
