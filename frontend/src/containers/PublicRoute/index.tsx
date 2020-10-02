f4f5a6bd5c98b3c7561a41d1cf4bdde918ec57e4
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-10-02 23:24:08 +0300

    added template for PublicRoute; added routes for login and register pages

---
+++ frontend/src/containers/PublicRoute/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { Route } from 'react-router-dom';
+
+export interface IPublicRouteProps {
+  component: any,
+  exact: boolean,
+  path: string
+}
+
+const PublicRoute: FC<IPublicRouteProps> = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={() => (
+        <>
+          <Component />
+        </>
+      )}
+    />
+  );
+};
+
+export default PublicRoute;
--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../PrivateRoute';
+import PublicRoute from '../PublicRoute';
 import MainPage from '../MainPage';
+import LoginPage from '../../components/LoginPage';
+import RegisterPage from '../../components/RegisterPage';
 
 const Routing: FC = () => {
 
   return (
     <div>
       <Switch>
+        <PublicRoute exact path="/login" component={LoginPage} />
+        <PublicRoute exact path="/register" component={RegisterPage} />
         <PrivateRoute exact path="/" component={() => <span>Landing page</span>} />
         <PrivateRoute exact path="/home" component={MainPage} />
         <Route path="/*" component={() => <span>Error</span>} />
