9b8055ab845a3eb0aaa46aaff1b2ff8d47e0a1a2
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2020-10-03 23:13:19 +0300

    added LoginPage basic structure

--- frontend/src/components/LoginPage/index.tsx
+++ frontend/src/components/LoginPage/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   return (
     <div>
-
+      <div>
+        <div>
+          <span>Welcome back!</span>
+          <span>To keep connected with us please login with your personal info</span>
+          <span>Need an account?</span>
+          <Link to="/register">Register...</Link>
+        </div>
+        <div>
+          <span>Login into Eshka</span>
+          <form>
+            <label>Email</label>
+            <input/>
+            <label>Password</label>
+            <input/>
+            <button>Sign in</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
