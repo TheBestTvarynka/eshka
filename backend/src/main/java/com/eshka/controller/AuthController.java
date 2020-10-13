2c3e0631c027b526af30155734b85bf40acf06cb
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2020-10-13 23:24:13 +0300

    configured security with SecurityConfig.java class; added AuthController

---
+++ backend/src/main/java/com/eshka/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.eshka.controller;
+
+import com.eshka.dto.auth.LoginDTO;
+import com.eshka.dto.auth.RegisterDTO;
+import com.eshka.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> login(@RequestBody LoginDTO credentials) {
+        return this.authService.login(credentials);
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> register(@RequestBody RegisterDTO registerData) {
+        return this.authService.register(registerData);
+    }
+
+}
