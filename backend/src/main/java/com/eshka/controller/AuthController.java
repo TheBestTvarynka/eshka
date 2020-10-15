ce916c5ffb09d63858ee75357021e76c393b1990
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2020-10-15 12:52:23 +0300

    added more exceptions and handler for it

--- backend/src/main/java/com/eshka/controller/AuthController.java
+++ backend/src/main/java/com/eshka/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.eshka.controller;
 import com.eshka.dto.auth.LoginDTO;
 import com.eshka.dto.auth.AfterLoginDTO;
 import com.eshka.dto.auth.RegisterDTO;
-import com.eshka.dto.request.UserDTO;
 import com.eshka.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,8 +26,8 @@ public class AuthController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> register(@RequestBody RegisterDTO registerData) {
-        return this.authService.register(registerData);
+    public void register(@RequestBody RegisterDTO registerData) {
+        this.authService.register(registerData);
     }
 
 }
--- backend/src/main/java/com/eshka/service/AuthService.java
+++ backend/src/main/java/com/eshka/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.eshka.service;
 
 import com.eshka.config.security.AppProperties;
+import com.eshka.config.security.AuthCookieFilter;
 import com.eshka.dto.auth.LoginDTO;
 import com.eshka.dto.auth.AfterLoginDTO;
 import com.eshka.dto.auth.RegisterDTO;
@@ -8,10 +9,13 @@ import com.eshka.entity.User;
 import com.eshka.entity.UserSession;
 import com.eshka.entity.enums.Role;
 import com.eshka.exception.UserAlreadyExist;
+import com.eshka.exception.UserNotExistException;
+import com.eshka.exception.WrongCredentialsException;
 import com.eshka.repository.UserRepository;
 import com.eshka.repository.UserSessionRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -31,7 +35,7 @@ public class AuthService {
     public ResponseEntity<AfterLoginDTO> login(LoginDTO credentials) {
         Optional<User> userOptional = userRepository.findByUsername(credentials.getUsername());
         if (userOptional.isEmpty()) {
-            return null;
+            throw new UserNotExistException();
         }
         User user = userOptional.get();
         if (passwordEncoder.matches(credentials.getPassword(), user.getPassword())) {
@@ -46,32 +50,40 @@ public class AuthService {
                     .build();
             sessionRepository.save(userSession);
 
-	    AfterLoginDTO afterLogin = AfterLoginDTO.builder()
-		    .fullName(user.getFullName())
-		    .username(user.getUsername())
-		    .email(user.getEmail())
-            .role(user.getRole())
-		    .sessionId(sessionId)
-		    .build();
-            return ResponseEntity.ok()
-                    .body(afterLogin);
+            ResponseCookie cookie = ResponseCookie
+                    .from(AuthCookieFilter.COOKIE_NAME, sessionId)
+                    .maxAge(this.appProperties.getCookieMaxAge())
+                    .sameSite("Strict")
+                    .path("/").httpOnly(true)
+                    .secure(this.appProperties.isSecureCookie())
+                    .build();
+            AfterLoginDTO afterLogin = AfterLoginDTO.builder()
+                .fullName(user.getFullName())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .sessionId(sessionId)
+                .build();
+                return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+                        .body(afterLogin);
+        } else {
+            throw new WrongCredentialsException();
         }
-        return null;
     }
 
-    public ResponseEntity<String> register(RegisterDTO registerData) {
+    public void register(RegisterDTO registerData) {
         if (userRepository.findByUsername(registerData.getUsername()).isPresent()) {
             throw new UserAlreadyExist("User already exist");
         }
         User user = User.builder()
                 .fullName(registerData.getFullName())
                 .username(registerData.getUsername())
+                .email(registerData.getEmail())
                 .password(passwordEncoder.encode(registerData.getPassword()))
                 .active(true)
                 .role(Role.USER)
                 .build();
         userRepository.save(user);
-        return new ResponseEntity<>("registered", HttpStatus.OK);
     }
 
 }
