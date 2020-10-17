d1c77979b588a22fa44fa6f7ff70c31fce3ad677
KySo4oK <prostorostuk08@gmail.com>
2020-10-17 22:26:07 +0300

    add subject repository

---
+++ backend/src/main/java/com/eshka/repository/SubjectRepository.java
@@ -0,0 +1,9 @@
+package com.eshka.repository;
+
+import com.eshka.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
