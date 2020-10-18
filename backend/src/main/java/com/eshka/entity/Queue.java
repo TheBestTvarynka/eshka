205377ff665b0c8f1622cd2789c96aaa3eab66c4
KySo4oK <prostorostuk08@gmail.com>
2020-10-18 22:48:17 +0300

    add primitive mapping tags for queue

--- backend/src/main/java/com/eshka/entity/Queue.java
+++ backend/src/main/java/com/eshka/entity/Queue.java
@@ -4,19 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "queue")
 public class Queue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @Column(name = "title", unique = true, nullable = false)
     private String title;
+    @Column(name = "description")
     private String description;
+    @Column(name = "creation_date", columnDefinition = "TIMESTAMP")
     private LocalDateTime creationDate;
+    @Column(name = "closing_date", columnDefinition = "TIMESTAMP")
     private LocalDateTime closingDate;
+    @Column(name = "start_date", columnDefinition = "TIMESTAMP")
     private LocalDateTime startDate;
+    @Column(name = "end_date", columnDefinition = "TIMESTAMP")
     private LocalDateTime endDate;
-    private long makerId;
-    private long subjectId;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "maker_id")
+    private User maker;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "subject_id")
+    private Subject subject;
 }
