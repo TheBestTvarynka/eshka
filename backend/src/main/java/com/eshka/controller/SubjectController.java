9ee0fd21539adc5045a0ed5851ba199235cc90c0
KySo4oK <prostorostuk08@gmail.com>
2020-10-18 17:53:05 +0300

    fix get method for subjects

--- backend/src/main/java/com/eshka/controller/SubjectController.java
+++ backend/src/main/java/com/eshka/controller/SubjectController.java
@@ -1,7 +1,10 @@
 package com.eshka.controller;
 
-import com.eshka.entity.Subject;
+import com.eshka.dto.response.SubjectResponse;
+import com.eshka.mapper.SubjectMapper;
 import com.eshka.service.SubjectService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +13,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("process all operations with subject objects")
 @RestController
 @RequestMapping("/subject")
 @RequiredArgsConstructor
 public class SubjectController {
     private final SubjectService subjectService;
+    private static final SubjectMapper mapper = SubjectMapper.INSTANCE;
 
+    @ApiOperation("get subject by id")
     @GetMapping("/{id}")
-    public ResponseEntity<Subject> findById(@PathVariable(name = "id") String id) {
-        return new ResponseEntity<>(subjectService.findById(Long.parseLong(id)), HttpStatus.OK);
+    public ResponseEntity<SubjectResponse> findById(@PathVariable(name = "id") String id) {
+        return new ResponseEntity<>(mapper.subjectToSubjectResponse(subjectService.findById(Long.parseLong(id))),
+                HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/eshka/service/SubjectService.java
+++ backend/src/main/java/com/eshka/service/SubjectService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class SubjectService {
-    private SubjectRepository subjectRepository;
+    private final SubjectRepository subjectRepository;
 
     public Subject findById(long id) {
         return subjectRepository.findById(id).orElseThrow();
