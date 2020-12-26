fa7702361ec4dc19eb4e05a6603cfaa849e3ba1e
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2020-12-26 19:57:40 +0200

    feat(dashboard): add join to team button

--- frontend/src/containers/Dashboard/index.tsx
+++ frontend/src/containers/Dashboard/index.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { IAppState } from '../../models/appState';
 import { loadTeamsRoutine } from '../../sagas/team/routines';
 import styles from './styles.module.sass';
 import { useHistory } from 'react-router-dom';
+import buttons from '../../components/styles/buttons.module.sass';
+import JoinTeamWindow from '../../components/JoinTeamWindow';
 
 const Dashboard: React.FC<IDashboardProps> = ({ teams, loadTeams }) => {
   const history = useHistory();
+  const [jw, setJW] = useState<boolean>(false);
 
   useEffect(() => {
     loadTeams();
@@ -20,6 +23,17 @@ const Dashboard: React.FC<IDashboardProps> = ({ teams, loadTeams }) => {
           <div className={styles.card_description}>{team.description}</div>
         </div>
       )}
+      <div className={styles.button_container}>
+        <button className={buttons.animated_border_button} onClick={() => setJW(true)}>
+          <span>Join</span>
+        </button>
+      </div>
+      {jw && <JoinTeamWindow onClose={() => setJW(false)}
+                             onSuccess={() => {
+                               setJW(false);
+                               if (loadTeams) loadTeams();
+                             }}
+      /> }
     </div>
   );
 };
