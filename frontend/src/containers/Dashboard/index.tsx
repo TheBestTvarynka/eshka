c92188dc01cc079bc8312c531d6c85bb7df6e16e
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2020-12-26 20:47:54 +0200

    feat(dashboard): add more content

--- frontend/src/containers/Dashboard/index.tsx
+++ frontend/src/containers/Dashboard/index.tsx
@@ -5,6 +5,8 @@ import { loadTeamsRoutine } from '../../sagas/team/routines';
 import styles from './styles.module.sass';
 import { useHistory } from 'react-router-dom';
 import buttons from '../../components/styles/buttons.module.sass';
+import containers from '../../components/styles/containers.module.sass';
+import lists from '../../components/styles/lists.module.sass';
 import JoinTeamWindow from '../../components/JoinTeamWindow';
 
 const Dashboard: React.FC<IDashboardProps> = ({ teams, loadTeams }) => {
@@ -16,24 +18,40 @@ const Dashboard: React.FC<IDashboardProps> = ({ teams, loadTeams }) => {
   }, [loadTeams]);
 
   return (
-    <div className={styles.card_container}>
-      {teams && teams.map(team =>
-        <div key={team.id} className={styles.card} onClick={() => history.push(`/team/${team.id}`)}>
-          <span className={styles.card_title}>{team.name}</span>
-          <div className={styles.card_description}>{team.description}</div>
+    <div>
+      <span className={styles.section_title}>All your teams:</span>
+      <div className={styles.card_container}>
+        {teams && teams.map(team =>
+          <div key={team.id} className={styles.card} onClick={() => history.push(`/team/${team.id}`)}>
+            <span className={styles.card_title}>{team.name}</span>
+            <div className={styles.card_description}>{team.description}</div>
+          </div>
+        )}
+        <div className={styles.button_container}>
+          <button className={buttons.animated_border_button} onClick={() => setJW(true)}>
+            <span>Join</span>
+          </button>
         </div>
-      )}
-      <div className={styles.button_container}>
-        <button className={buttons.animated_border_button} onClick={() => setJW(true)}>
-          <span>Join</span>
-        </button>
+        {jw && <JoinTeamWindow onClose={() => setJW(false)}
+                               onSuccess={() => {
+                                 setJW(false);
+                                 if (loadTeams) loadTeams();
+                               }}
+        /> }
       </div>
-      {jw && <JoinTeamWindow onClose={() => setJW(false)}
-                             onSuccess={() => {
-                               setJW(false);
-                               if (loadTeams) loadTeams();
-                             }}
-      /> }
+      <span className={styles.section_title}>Last events & updates:</span>
+      <div className={styles.events_container}>
+        <div className={containers.vertical_actions_panel}>
+          <span className={styles.list_item}>You joined to a team 'IP-82'</span>
+          <span className={styles.list_item}>Maks joined to the team 'IP-82'</span>
+          <span className={styles.list_item}>Stepan leave the team 'IP-82'</span>
+        </div>
+        <div className={containers.vertical_actions_panel}>
+          <span className={styles.list_item}>Queue 'Lab6' in Java subject opened</span>
+          <span className={styles.list_item}>A few minutes left until queue 'Lab4' will be closed</span>
+          <span className={styles.list_item}>Sasha recently create subject ''</span>
+        </div>
+      </div>
     </div>
   );
 };
